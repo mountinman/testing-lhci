78b56d0a0cb3e304aa252f517113bae648d13542
MarkoCvrljak <marko.cvrljak1@gmail.com>
2022-01-18 22:51:11 +0100

    blah

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,8 +2,14 @@ module.exports = {
     ci: {
       collect: {
         startServerCommand: 'npm run dev',
-        url: ['http://localhost:3000/'],
-        numberOfRuns: 5,
+        url: ['http://localhost:3000/', 'http://localhost:3000/about'],
+        numberOfRuns: 1,
+      },
+      assert: {
+        assertions: {
+          'categories:performance': ['error', { minScore: 0.6 }],
+          'categories:accessibility': ['warn', { minScore: 0.9 }],
+        },
       },
     },
   };
